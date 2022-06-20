00672322f9c3a6b7a625b3dacf3556e82c3e2057
jimmyoops <jimmy85030@gmail.com>
2022-06-20 09:13:18 +0800

    [Amend] rename files [Create] species user flow

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,14 +6,20 @@ const { API_VERSION } = process.env
 const api = express.Router();
 
 const {
-    trackRouter
-} = require('./createmap_router')
+    trackerRouter
+} = require('./tracker_router')
+const {
+    speciesRouter
+} = require('./species_router')
 const {
     getDataAllRouter,
     getDataGPSRouter
 } = require('./sightdata_router')
 
-api.use(`/${API_VERSION}/track`, trackRouter)
+
+api.use(`/${API_VERSION}/tracker`, trackerRouter)
+api.use(`/${API_VERSION}/species`, speciesRouter)
 api.use(`/${API_VERSION}/data`, getDataAllRouter, getDataGPSRouter)
+
 
 module.exports = api;
