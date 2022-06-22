7fc0f20f154ff9dd8ca371d39a0d36d33dd64d41
jimmyoops <jimmy85030@gmail.com>
2022-06-22 11:15:44 +0800

    [Amend] according to DB Schema refactor

--- server/models/sightdata_model.js
+++ server/models/sightdata_model.js
@@ -1,41 +1,36 @@
 const { queryPromise } = require('../../utils/mysql');
 
 const getDataAll = async (pageSize, paging = 0, requirement = {}) => {
-    const condition = {sql: '', binding: []};
-    const limit = {
-        sql: 'LIMIT ?, ?',
-        binding: [pageSize * paging, pageSize]
-    };
-
-    const dataQuery = 'SELECT * FROM basic_info ' + 
-    `INNER JOIN sighting ON 
-    basic_info.id = sighting.basicinfo_id
-    INNER JOIN obv_approach ON
-    sighting.id = obv_approach.sighting_id
-	INNER JOIN obv_detail ON
-    sighting.id = obv_detail.sighting_id
-	INNER JOIN obv_gps ON
-    sighting.id = obv_gps.sighting_id
-	INNER JOIN obv_interaction ON
-    sighting.id = obv_interaction.sighting_id `
-     + 'ORDER BY basic_info.sailing_id DESC';
+    // const condition = {sql: '', binding: []};
+    // const limit = {
+    //     sql: 'LIMIT ?, ?',
+    //     binding: [pageSize * paging, pageSize]
+    // };
+    const dataQuery = 'SELECT * FROM sailing_info ' + 
+    `INNER JOIN obv_detail 
+    ON sailing_info.id = obv_detail.obv_id
+    INNER JOIN obv_approach
+    ON sailing_info.id = obv_approach.obv_id
+	INNER JOIN obv_gps 
+    ON sailing_info.id = obv_gps.obv_id
+	INNER JOIN obv_interaction 
+    ON sailing_info.id = obv_interaction.obv_id `
+     + 'ORDER BY sailing_info.id ASC';
     const data = await queryPromise(dataQuery)  
     return data
 };
 
 const getDataGPS = async (pageSize, paging = 0, requirement = {}) => {
-    const condition = {sql: '', binding: []};
-    const limit = {
-        sql: 'LIMIT ?, ?',
-        binding: [pageSize * paging, pageSize]
-    };
+    // const condition = {sql: '', binding: []};
+    // const limit = {
+    //     sql: 'LIMIT ?, ?',
+    //     binding: [pageSize * paging, pageSize]
+    // };
 
-    const dataQuery = 'SELECT sailing_id, latitude, latitude_min, latitude_sec, longtitude, longtitude_min, longtitude_sec FROM obv_gps ' + 
-    `INNER JOIN sighting ON
-    obv_gps.sighting_id = sighting.id
-    INNER JOIN basic_info ON
-    sighting.basicinfo_id = basic_info.id
-    ` + 'ORDER BY obv_gps.sighting_id ASC';
+    const dataQuery = 'SELECT obv_id, sailing_id, year, month, day, , period, sighting_id, mix weather, wind_direction, current, latitude, latitude_min, latitude_sec, longitude, longitude_min, longitude_sec FROM sailing_info ' + 
+    `INNER JOIN obv_gps ON
+    sailing_info.id = obv_gps.obv_id 
+    ` + 'ORDER BY obv_gps.obv_id ASC';
 
     const data = await queryPromise(dataQuery);
     data
