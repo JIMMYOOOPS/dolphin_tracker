7a36855ac2d90902a36c1901e3a22d16c4de5e50
jimmyoops <jimmy85030@gmail.com>
2022-07-14 12:57:49 +0800

    [Amend] Check infinite scroll

--- public/src/species.js
+++ public/src/species.js
@@ -64,19 +64,18 @@
             });
 
             let infScroll = $('.intro').data('infiniteScroll');
-            
-
             $('.intro').on( 'load.infiniteScroll', function( event, data ) {
                 if (infScroll.pageIndex < 7) {
                     dolphinData = data.data;
                     pageNum = infScroll.pageIndex;
+                    console.log(pagNum);
                     pageSize = 4;
                     let numInfos = pageNum * pageSize;
                     createInfoCard(numInfos, pageSize);
                     insertInfoCard(numInfos, pageSize);
                 }
             })
-            $('.intro').infiniteScroll('loadNextPage');
+            .infiniteScroll('loadNextPage');
         } catch (err) {
             console.log(err);
         }
